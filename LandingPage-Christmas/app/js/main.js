5800fbb74ed13ca81b6fab8fac263aa968adcd0d
VOVA-PC\Vova <vova.moskalyuk11jr@gmail.com>
2021-01-04 14:39:47 +0200

    add test page

--- LandingPage-Christmas/app/js/main.js
+++ LandingPage-Christmas/app/js/main.js
@@ -1,4 +1,4 @@
-// =============== show menu
+/*==================== SHOW MENU ====================*/
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
@@ -6,7 +6,6 @@ const showMenu = (toggleId, navId) => {
   // Validate that variables exist
   if (toggle && nav) {
     toggle.addEventListener('click', () => {
-      console.log('клік на меню');
       // We add the show-menu class to the div tag with the nav__menu class
       nav.classList.toggle('show-menu');
     });
@@ -14,8 +13,7 @@ const showMenu = (toggleId, navId) => {
 };
 showMenu('nav-toggle', 'nav-menu');
 
-// =============== remove menu mob.
-
+/*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
@@ -25,8 +23,7 @@ function linkAction() {
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-// =============== scroll to section active link
-
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]');
 
 function scrollActive() {
@@ -58,3 +55,76 @@ function scrollHeader() {
   else nav.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader);
+
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
+  const scrollTop = document.getElementById('scroll-top');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollTop.classList.add('show-scroll');
+  else scrollTop.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollTop);
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-toggle-right';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme)
+    ? 'bx-toggle-left'
+    : 'bx-toggle-right';
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme,
+  );
+  themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](
+    iconTheme,
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+  distance: '30px',
+  duration: 1800,
+  reset: true,
+});
+
+sr.reveal(
+  `.home__data, .home__img, 
+         .decoration__data,
+         .accessory__content,
+         .footer__content`,
+  {
+    origin: 'top',
+    interval: 200,
+  },
+);
+
+sr.reveal(`.share__img, .send__content`, {
+  origin: 'left',
+});
+
+sr.reveal(`.share__data, .send__img`, {
+  origin: 'right',
+});
